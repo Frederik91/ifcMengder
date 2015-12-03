a0cd2c2fbebbcd34fa475899fed9c2dab09289a7
Frederik91 <frederik.t91@gmail.com>
2015-12-03 19:58:56 +0100

    Ikke kommet noe videre...

---
+++ IfcExtractor/IfcExtractor/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xbim.IO;
+
+namespace IfcExtractor
+{
+    public class Class1
+    {
+        public void readIfcFile()
+        {           
+
+            XbimModel model = XbimModel.CreateModel("newModel");
+            model.CreateFrom(@"C:\Users\Frederik\Source\Repos\ifcMengder\IfcExtractor\IfcExtractor\SB_1017801_7_RIE_430_00.ifc");
+
+        }
+
+    }
+}
---
+++ IfcExtractor/IfcExtractorTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IfcExtractor;
+
+namespace IfcExtractorTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ReadIfcTest()
+        {
+            var X = new Class1();
+
+            X.readIfcFile();
+        }
+    }
+}
