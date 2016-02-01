9ef51a69910803bfee89a6c82002096b16533999
Frederik91 <frederik.t91@gmail.com>
2016-02-01 18:52:37 +0100

    First sync

---
+++ ArduinoBil/ArduinoBil/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace ArduinoBil
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toggleConnectBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.cbPorts = new System.Windows.Forms.ComboBox();
+            this.SpeedBar = new System.Windows.Forms.TrackBar();
+            this.ServoSlider = new System.Windows.Forms.TrackBar();
+            this.SpeedBox = new System.Windows.Forms.TextBox();
+            this.AngleBox = new System.Windows.Forms.TextBox();
+            this.SetAngleButton = new System.Windows.Forms.Button();
+            this.setSpeedButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.SpeedBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ServoSlider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toggleConnectBtn
+            // 
+            this.toggleConnectBtn.Location = new System.Drawing.Point(139, 9);
+            this.toggleConnectBtn.Name = "toggleConnectBtn";
+            this.toggleConnectBtn.Size = new System.Drawing.Size(104, 23);
+            this.toggleConnectBtn.TabIndex = 0;
+            this.toggleConnectBtn.Text = "Connect";
+            this.toggleConnectBtn.UseVisualStyleBackColor = true;
+            this.toggleConnectBtn.Click += new System.EventHandler(this.toggleConnectBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(467, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Status:";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Location = new System.Drawing.Point(525, 12);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(94, 17);
+            this.StatusLabel.TabIndex = 2;
+            this.StatusLabel.Text = "Disconnected";
+            // 
+            // cbPorts
+            // 
+            this.cbPorts.FormattingEnabled = true;
+            this.cbPorts.Location = new System.Drawing.Point(12, 9);
+            this.cbPorts.Name = "cbPorts";
+            this.cbPorts.Size = new System.Drawing.Size(121, 24);
+            this.cbPorts.TabIndex = 3;
+            this.cbPorts.SelectedIndexChanged += new System.EventHandler(this.cbPorts_SelectedIndexChanged);
+            // 
+            // SpeedBar
+            // 
+            this.SpeedBar.Location = new System.Drawing.Point(12, 39);
+            this.SpeedBar.Maximum = 255;
+            this.SpeedBar.Name = "SpeedBar";
+            this.SpeedBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.SpeedBar.Size = new System.Drawing.Size(56, 243);
+            this.SpeedBar.TabIndex = 4;
+            this.SpeedBar.Scroll += new System.EventHandler(this.SpeedBar_Scroll);
+            // 
+            // ServoSlider
+            // 
+            this.ServoSlider.Location = new System.Drawing.Point(74, 118);
+            this.ServoSlider.Maximum = 179;
+            this.ServoSlider.Name = "ServoSlider";
+            this.ServoSlider.Size = new System.Drawing.Size(243, 56);
+            this.ServoSlider.TabIndex = 5;
+            this.ServoSlider.Scroll += new System.EventHandler(this.ServoSlider_Scroll);
+            // 
+            // SpeedBox
+            // 
+            this.SpeedBox.Location = new System.Drawing.Point(61, 246);
+            this.SpeedBox.Name = "SpeedBox";
+            this.SpeedBox.Size = new System.Drawing.Size(63, 22);
+            this.SpeedBox.TabIndex = 6;
+            this.SpeedBox.TextChanged += new System.EventHandler(this.SpeedBox_TextChanged);
+            // 
+            // AngleBox
+            // 
+            this.AngleBox.Location = new System.Drawing.Point(157, 90);
+            this.AngleBox.Name = "AngleBox";
+            this.AngleBox.Size = new System.Drawing.Size(63, 22);
+            this.AngleBox.TabIndex = 7;
+            this.AngleBox.TextChanged += new System.EventHandler(this.AngleBox_TextChanged);
+            // 
+            // SetAngleButton
+            // 
+            this.SetAngleButton.Location = new System.Drawing.Point(227, 89);
+            this.SetAngleButton.Name = "SetAngleButton";
+            this.SetAngleButton.Size = new System.Drawing.Size(63, 23);
+            this.SetAngleButton.TabIndex = 8;
+            this.SetAngleButton.Text = "Set";
+            this.SetAngleButton.UseVisualStyleBackColor = true;
+            this.SetAngleButton.Click += new System.EventHandler(this.SetAngleButton_Click);
+            // 
+            // setSpeedButton
+            // 
+            this.setSpeedButton.Location = new System.Drawing.Point(139, 245);
+            this.setSpeedButton.Name = "setSpeedButton";
+            this.setSpeedButton.Size = new System.Drawing.Size(63, 23);
+            this.setSpeedButton.TabIndex = 9;
+            this.setSpeedButton.Text = "Set";
+            this.setSpeedButton.UseVisualStyleBackColor = true;
+            this.setSpeedButton.Click += new System.EventHandler(this.setSpeedButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 280);
+            this.Controls.Add(this.setSpeedButton);
+            this.Controls.Add(this.SetAngleButton);
+            this.Controls.Add(this.AngleBox);
+            this.Controls.Add(this.SpeedBox);
+            this.Controls.Add(this.ServoSlider);
+            this.Controls.Add(this.SpeedBar);
+            this.Controls.Add(this.cbPorts);
+            this.Controls.Add(this.StatusLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toggleConnectBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.SpeedBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ServoSlider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button toggleConnectBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.ComboBox cbPorts;
+        private System.Windows.Forms.TrackBar SpeedBar;
+        private System.Windows.Forms.TrackBar ServoSlider;
+        private System.Windows.Forms.TextBox SpeedBox;
+        private System.Windows.Forms.TextBox AngleBox;
+        private System.Windows.Forms.Button SetAngleButton;
+        private System.Windows.Forms.Button setSpeedButton;
+    }
+}
+
---
+++ ArduinoBil/ArduinoBil/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArduinoBil
+{
+    public partial class Form1 : Form
+    {
+        public enum ArduinoStatus
+        {
+            Connected,
+            Connecting,
+            Disconnected
+        }
+
+        private ArduinoStatus StatusEnum = ArduinoStatus.Disconnected;
+        private SerialPort _arduinoPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+            cbPorts.Items.AddRange(SerialPort.GetPortNames());
+            cbPorts.SelectedIndex = 0;
+
+        }
+
+        private void toggleConnectBtn_Click(object sender, EventArgs e)
+        {
+            switch (StatusEnum)
+            {
+                case ArduinoStatus.Connected:
+                    try
+                    {
+                        setMotorSpeed(0);
+
+                        _arduinoPort.Close();
+                        StatusLabel.Text = "Disconnected";
+                        toggleConnectBtn.Text = "Connect";
+                        StatusEnum = ArduinoStatus.Disconnected;
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        StatusLabel.Text = "Error";
+                    }
+                    break;
+                case ArduinoStatus.Connecting:
+                    break;
+                case ArduinoStatus.Disconnected:
+                    StatusEnum = ArduinoStatus.Connecting;
+                    _arduinoPort = new SerialPort(cbPorts.SelectedItem.ToString());
+                    _arduinoPort.BaudRate = 9600;
+
+                    try
+                    {
+                        _arduinoPort.Open();
+                        setMotorSpeed(SpeedBar.Value);
+                        StatusEnum = ArduinoStatus.Connected;
+                        StatusLabel.Text = "Connected";
+                        toggleConnectBtn.Text = "Disconnect";
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        StatusLabel.Text = "Error";
+                    }
+
+                    break;
+            }
+
+        }
+
+        private void cbPorts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SpeedBar_Scroll(object sender, EventArgs e)
+        {
+            if (StatusEnum == ArduinoStatus.Connected)
+            {
+                setMotorSpeed(SpeedBar.Value);
+                SpeedBox.Text = SpeedBar.Value.ToString();
+            }
+        }
+
+        private void setMotorSpeed(int speed)
+        {
+            byte[] buffer2 = new byte[2];
+            buffer2[0] = 16;
+            buffer2[1] = (byte)speed;
+            _arduinoPort.Write(buffer2, 0, 2);
+            Thread.Sleep(15);
+
+        }
+
+        private void setServoPosition(int angle)
+        {
+            byte[] buffer2 = new byte[2];
+            buffer2[0] = 32;
+            buffer2[1] = (byte)angle;
+            _arduinoPort.Write(buffer2, 0, 2);
+            Thread.Sleep(15);
+
+        }
+
+        private void ServoSlider_Scroll(object sender, EventArgs e)
+        {
+            if (StatusEnum == ArduinoStatus.Connected)
+            {
+                setServoPosition(ServoSlider.Value);
+                AngleBox.Text = ServoSlider.Value.ToString();
+            }
+        }
+
+        private void SpeedBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AngleBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void setSpeedButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var value = Convert.ToInt32(SpeedBox.Text);
+                setMotorSpeed(value);
+                SpeedBar.Value = value;
+            }
+            catch (Exception)
+            {
+                SpeedBox.Text = "Invalid";
+            }
+        }
+
+        private void SetAngleButton_Click(object sender, EventArgs e)
+        {
+            var value = Convert.ToInt32(AngleBox.Text);
+            try
+            {
+                setServoPosition(value);
+                ServoSlider.Value = value;
+            }
+            catch (Exception)
+            {
+                AngleBox.Text = "Invalid";
+            }
+        }
+    }
+}
